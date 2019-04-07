3230f60860d05bfae9cc103316f5554b541f24e2
Juan Garcia <juangarcia.ts@gmail.com>
2019-04-07 11:56:59 -0300

    Criação do endpoint de obterHistorico

--- server.py
+++ server.py
@@ -4,9 +4,10 @@ from models.grupo_model import Grupo
 from models.tipo_model import Tipo
 from models.titulo_crawler_model import TituloCrawler
 from models.historico_titulos_model import HistoricoTitulos
-from flask_api import FlaskAPI
-from flask_api import status
+from flask import request, url_for
+from flask_api import FlaskAPI, status
 from flask_cors import CORS
+from bson import json_util
 import time
 import os
 
@@ -39,12 +40,39 @@ def listar_tipos():
 
 @app.route('/obtertitulosatualizados', methods=['GET'])
 def obter_titulos_atualizados():  
-  historico = HistoricoTitulos.objects.order_by('-data_extracao').first()
-  ultima_extracao = HistoricoTitulos.objects.order_by('-id').first()
+  titulos = HistoricoTitulos.objects.order_by('-data_extracao').first()
 
-  return historico.to_json(), status.HTTP_200_OK
+  return titulos.to_json(), status.HTTP_200_OK
+
+@app.route('/obterhistorico/', methods=['POST'])
+def obter_historico():
+  tipo = request.data['tipo']
+  nome = request.data['nome']
+  
+  result = []
+
+  pipeline = [
+    { "$unwind": '$lista_titulos' },
+    { "$match": { "lista_titulos.nome_titulo": nome, "lista_titulos.tipo_titulo.tipo": tipo } },
+    { "$group": { "_id": "$data_extracao", "preco": { "$first": "$lista_titulos.preco_unitario" } } }, 
+    { "$sort": { "_id": 1 } }   
+  ]
+
+  historico = HistoricoTitulos.objects.aggregate(*pipeline)
+
+  for h in historico:
+    result.append(h)
+
+  if len(result) > 30:
+    result = result[:30]    
+
+  result = json_util.dumps(result)  
+
+  return json_util.loads(result,
+  ), status.HTTP_200_OK
 
 # Rodar servidor
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
