d44aa0536cec121e163f1bf74e94719b1a21beae
Juan Garcia Trindade E Silva <juan.garcia.trindade@ibm.com>
2019-06-06 16:17:22 -0300

    Envio de SMS/Email

--- models/alerta_model.py
+++ models/alerta_model.py
@@ -2,52 +2,91 @@ from mongoengine import *
 import mongoengine_goodjson as gj
 from models.tipo_model import Tipo
 from twilio.rest import Client
+from urllib.parse import urlencode
+import requests
+import httplib2
 import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+import json
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+# Email (MailGun)
+MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY",
+                                 config["mailgun"]["api_key"])
+MAILGUN_DOMAIN_NAME = os.environ.get("MAILGUN_DOMAIN_NAME",
+                                     config["mailgun"]["domain_name"])
+
+# SMS (Twilio)
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID",
+                                    config["twilio"]["account_sid"])
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN",
+                                   config["twilio"]["auth_token"])
+TWILIO_PHONE_NUMBER = os.environ.get("TWILIO_PHONE_NUMBER",
+                                     config["twilio"]["phone_number"])
+
 
 class Alerta(gj.Document):
     usuario_id = StringField(required=True)
     nome_titulo = StringField(required=True)
     grupo_titulo = IntField(required=True)
     tipo_titulo = IntField(required=True)
-    tipo_notificacao = StringField(required=True) # "SMS" ou "EMAIL"
-    situacao = IntField(required=True) # -1 menor, 0 igual ou  1 maior
+    tipo_notificacao = StringField(required=True)  # "SMS" ou "EMAIL"
+    situacao = IntField(required=True)  # -1 menor, 0 igual ou  1 maior
     valor = DecimalField(required=True)
     enviado = BooleanField(required=True, default=False)
 
-    def send_alert(self, contatc):
-        if tipo_notificacao:
-            alerta_smtp(contatc, nome_titulo)
+    def obter_mensagem(self):
+        if (self.situacao == -1):
+            situacao = "está abaixo de"
+        elif (self.situacao == 1):
+            situacao = "está acima de"
         else:
-            alerta_sms(contatc, nome_titulo)
+            situacao = "alcançou"
 
-    def alerta_smtp(email, titulo):
-        message = Mail(
-            from_email='augusto.assuncao@uniriotec.br',
-            to_emails= email,
-            subject= 'O seu alerta foi disparado',
-            html_content= '<strong>Seu alerta do titulo: {} foi acionado hoje.</strong>'.format(titulo)
-        )
-        try:
-            sg = SendGridAPIClient(os.environ.get('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'))
-            response = sg.send(message)
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
-        except Exception as e:
-            print(e.message)
+        return "Seu alerta para o titulo {} foi disparado! O valor {} R$ {}".format(
+            self.nome_titulo, situacao, self.valor)
 
-    def alerta_sms(telefone, nome_titulo):
-        account_sid = 'ACe11d51b65ee49ae0e5e20ee91255fff5'
-        auth_token = '9f2e1271e7d4b41f10dac1c9d5e4a434'
-        client = Client(account_sid, auth_token)
+    def send_alert(self, contact):
+        if self.tipo_notificacao == "EMAIL":
+            self.alerta_smtp(contact)
+        else:
+            self.alerta_sms(contact)
 
-        message = client.messages \
-                    .create(
-                        body="Seu alerta para o titulo: {} foi disparado!".format(nome_titulo),
-                        from_='+18282374201',
-                        to='+55{}'.format(telefone)
-                    )
+    def alerta_smtp(self, email):
+        http = httplib2.Http()
+        http.add_credentials('api', MAILGUN_API_KEY)
 
-        print(message.sid)
+        message = self.obter_mensagem()
+
+        url = 'https://api.mailgun.net/v3/{}/messages'.format(
+            MAILGUN_DOMAIN_NAME)
+
+        data = {
+            'from': 'Meu Tesouro <mailgun@{}>'.format(MAILGUN_DOMAIN_NAME),
+            'to': email,
+            'subject': '[Meu Tesouro] Um alerta foi disparado',
+            'text': message
+        }
+
+        resp, content = http.request(
+            url,
+            'POST',
+            urlencode(data),
+            headers={"Content-Type": "application/x-www-form-urlencoded"})
+
+        if resp.status != 200:
+            raise RuntimeError('Mailgun API error: {} {}'.format(
+                resp.status, content))
+
+    def alerta_sms(self, telefone):
+        client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+        situacao = ""
+
+        message = self.obter_mensagem()
+
+        sms = client.messages.create(body=message,
+                                     from_=TWILIO_PHONE_NUMBER,
+                                     to='+55{}'.format(telefone))
+
+        print(sms.sid)
--- server.py
+++ server.py
@@ -20,174 +20,233 @@ app = FlaskAPI(__name__)
 CORS(app)
 
 # Conexão com o MongoDB (Atlas)
-connect('tesouro-direto', host='mongodb+srv://admin:root@tesouro-direto-cwrre.mongodb.net/test?retryWrites=true')
+connect(
+    'tesouro-direto',
+    host=
+    'mongodb+srv://admin:root@tesouro-direto-cwrre.mongodb.net/test?retryWrites=true'
+)
+
 
 # Controllers
 @app.route('/listarpostagens', methods=['GET'])
 def listar_postagens():
-  postagens = PostagemBlog.objects.order_by('-data_inclusao')
+    postagens = PostagemBlog.objects.order_by('-data_inclusao')
 
-  return postagens.to_json(), status.HTTP_200_OK
+    return postagens.to_json(), status.HTTP_200_OK
+
 
 @app.route('/obterpostagem/<string:uid>', methods=['GET'])
 def obter_postagem(uid):
-  postagem = PostagemBlog.objects.get(pk=uid)
+    postagem = PostagemBlog.objects.get(pk=uid)
 
-  return postagem.to_json(), status.HTTP_200_OK
+    return postagem.to_json(), status.HTTP_200_OK
+
 
 @app.route('/adicionarpostagem', methods=['POST'])
 def adicionar_postagem():
-  destaque = request.data['isPostHighlight']
-  titulo = request.data['postTitle']
-  resumo = request.data['postSummary']
-  imagem_capa = request.data['postImage']
-  html = request.data['postHtml']
+    destaque = request.data['isPostHighlight']
+    titulo = request.data['postTitle']
+    resumo = request.data['postSummary']
+    imagem_capa = request.data['postImage']
+    html = request.data['postHtml']
 
-  postagem = PostagemBlog(destaque=destaque, titulo=titulo, resumo=resumo, imagem_capa=imagem_capa, html=html)
-  postagem.save()
+    postagem = PostagemBlog(destaque=destaque,
+                            titulo=titulo,
+                            resumo=resumo,
+                            imagem_capa=imagem_capa,
+                            html=html)
+    postagem.save()
 
-  return str(postagem.id), status.HTTP_200_OK
+    return str(postagem.id), status.HTTP_200_OK
+
 
 @app.route('/editarpostagem', methods=['POST'])
 def editarpostagem():
-   uid = request.data['postId']
-   destaque = request.data['isPostHighlight']
-   titulo = request.data['postTitle']
-   resumo = request.data['postSummary']
-   imagem_capa = request.data['postImage']
-   html = request.data['postHtml']
+    uid = request.data['postId']
+    destaque = request.data['isPostHighlight']
+    titulo = request.data['postTitle']
+    resumo = request.data['postSummary']
+    imagem_capa = request.data['postImage']
+    html = request.data['postHtml']
 
-   postagem = PostagemBlog.objects.get(pk=uid)
+    postagem = PostagemBlog.objects.get(pk=uid)
 
-   if postagem:
-      postagem.destaque = destaque
-      postagem.titulo = titulo
-      postagem.resumo = resumo
-      postagem.imagem_capa = imagem_capa
-      postagem.html = html
-      postagem.save()
+    if postagem:
+        postagem.destaque = destaque
+        postagem.titulo = titulo
+        postagem.resumo = resumo
+        postagem.imagem_capa = imagem_capa
+        postagem.html = html
+        postagem.save()
 
-   return str(postagem.id), status.HTTP_200_OK
+    return str(postagem.id), status.HTTP_200_OK
+
 
 @app.route('/removerpostagem', methods=['POST'])
 def remover_postagem():
-   uid = request.data['postId']
+    uid = request.data['postId']
 
-   postagem = PostagemBlog.objects.get(pk=uid)
+    postagem = PostagemBlog.objects.get(pk=uid)
 
-   if postagem:
-      postagem.delete()
+    if postagem:
+        postagem.delete()
 
-   return "OK", status.HTTP_200_OK
+    return "OK", status.HTTP_200_OK
+
 
 @app.route('/extrairdados', methods=['GET'])
 def extrair_dados():
-  start_time = time.time()
-  httpStatus = status.HTTP_200_OK if CrawlerBusiness.excract_data() == True else status.HTTP_500_INTERNAL_SERVER_ERROR
-  elapsed_time = time.time() - start_time
+    start_time = time.time()
+    httpStatus = status.HTTP_200_OK if CrawlerBusiness.excract_data(
+    ) == True else status.HTTP_500_INTERNAL_SERVER_ERROR
+    elapsed_time = time.time() - start_time
 
-  return str(elapsed_time), httpStatus
+    return str(elapsed_time), httpStatus
+
 
 @app.route('/listargrupos', methods=['GET'])
 def listar_grupos():
-  grupos = Grupo.objects.order_by('tipo')
+    grupos = Grupo.objects.order_by('tipo')
 
-  return grupos.to_json(), status.HTTP_200_OK
+    return grupos.to_json(), status.HTTP_200_OK
+
 
 @app.route('/listartipos', methods=['GET'])
 def listar_tipos():
-  tipos = Tipo.objects.order_by('tipo')
+    tipos = Tipo.objects.order_by('tipo')
 
-  return tipos.to_json(), status.HTTP_200_OK
+    return tipos.to_json(), status.HTTP_200_OK
+
 
 @app.route('/obterhistorico', methods=['POST'])
 def obter_historico():
-  tipo = request.data['tipo']
-  nome = request.data['nome']
+    tipo = request.data['tipo']
+    nome = request.data['nome']
 
-  result = []
+    result = []
 
-  pipeline = [
-    { "$unwind": '$lista_titulos' },
-    { "$match": { "lista_titulos.nome_titulo": nome, "lista_titulos.tipo_titulo.tipo": tipo } },
-    { "$group": { "_id": "$data_extracao", "preco": { "$first": "$lista_titulos.preco_unitario" } } },
-    { "$sort": { "_id": 1 } }
-  ]
+    pipeline = [{
+        "$unwind": '$lista_titulos'
+    }, {
+        "$match": {
+            "lista_titulos.nome_titulo": nome,
+            "lista_titulos.tipo_titulo.tipo": tipo
+        }
+    }, {
+        "$group": {
+            "_id": "$data_extracao",
+            "preco": {
+                "$first": "$lista_titulos.preco_unitario"
+            }
+        }
+    }, {
+        "$sort": {
+            "_id": 1
+        }
+    }]
 
-  historico = HistoricoTitulos.objects.aggregate(*pipeline)
+    historico = HistoricoTitulos.objects.aggregate(*pipeline)
 
-  for h in historico:
-    result.append(h)
+    for h in historico:
+        result.append(h)
 
-  if len(result) > 30:
-    result = result[:30]
+    if len(result) > 30:
+        result = result[:30]
 
-  result = json_util.dumps(result)
+    result = json_util.dumps(result)
 
-  return json_util.loads(result), status.HTTP_200_OK
+    return json_util.loads(result), status.HTTP_200_OK
+
 
 @app.route('/obtertitulosatualizados', methods=['GET'])
 def obter_titulos_atualizados():
-  titulos = HistoricoTitulos.objects.order_by('-data_extracao').first()
+    titulos = HistoricoTitulos.objects.order_by('-data_extracao').first()
 
-  return titulos.to_json(), status.HTTP_200_OK
+    return titulos.to_json(), status.HTTP_200_OK
+
 
 @app.route('/obterusuario/<string:firebase_id>', methods=['GET'])
 def obter_usuario(firebase_id):
-  usuario = Usuario.objects(firebase_id=firebase_id).first()
+    usuario = Usuario.objects(firebase_id=firebase_id).first()
 
-  return usuario.to_json(), status.HTTP_200_OK
+    return usuario.to_json(), status.HTTP_200_OK
+
 
 @app.route('/editarusuario', methods=['POST'])
 def editar_usuario():
-  firebase_id = request.data['firebase_id']  
-  foto = request.data['foto']
-  telefone = request.data['telefone'] 
+    firebase_id = request.data['firebase_id']
+    foto = request.data['foto']
+    telefone = request.data['telefone']
 
-  usuario = Usuario.objects(firebase_id=firebase_id).first()
+    usuario = Usuario.objects(firebase_id=firebase_id).first()
 
-  if usuario:
-      usuario.foto = foto
-      usuario.telefone = telefone
-  else:   
-      usuario = Usuario(firebase_id=firebase_id, foto=foto, telefone=telefone)    
+    if usuario:
+        usuario.foto = foto
+        usuario.telefone = telefone
+    else:
+        usuario = Usuario(firebase_id=firebase_id,
+                          foto=foto,
+                          telefone=telefone)
 
-  usuario.save()
+    usuario.save()
 
-  return usuario.to_json(), status.HTTP_200_OK
+    return usuario.to_json(), status.HTTP_200_OK
+
 
 @app.route('/obteralertas/<string:firebase_id>', methods=['GET'])
 def obter_alertas(firebase_id):
-  alertas = Alerta.objects(usuario_id=firebase_id).all()
+    alertas = Alerta.objects(usuario_id=firebase_id).all()
 
-  return alertas.to_json(), status.HTTP_200_OK
+    return alertas.to_json(), status.HTTP_200_OK
+
 
 @app.route('/adicionaralerta', methods=['POST'])
 def adicionar_alerta():
-  firebase_id = request.data['firebase_id']
-  nome_titulo = request.data['nome_titulo']
-  tipo_notificacao = request.data['tipo_notificacao']
-  situacao = request.data['situacao']
-  valor = request.data['valor']
-  grupo_titulo = request.data['grupo_titulo']
-  tipo_titulo = request.data['tipo_titulo']
+    firebase_id = request.data['firebase_id']
+    nome_titulo = request.data['nome_titulo']
+    tipo_notificacao = request.data['tipo_notificacao']
+    situacao = request.data['situacao']
+    valor = request.data['valor']
+    grupo_titulo = request.data['grupo_titulo']
+    tipo_titulo = request.data['tipo_titulo']
 
-  alerta = Alerta(usuario_id=firebase_id, nome_titulo=nome_titulo, grupo_titulo=grupo_titulo, tipo_titulo=tipo_titulo, tipo_notificacao=tipo_notificacao, situacao=situacao, valor=valor)
-  alerta.save()
+    alerta = Alerta(usuario_id=firebase_id,
+                    nome_titulo=nome_titulo,
+                    grupo_titulo=grupo_titulo,
+                    tipo_titulo=tipo_titulo,
+                    tipo_notificacao=tipo_notificacao,
+                    situacao=situacao,
+                    valor=valor)
+    alerta.save()
 
-  return str(alerta.id), status.HTTP_200_OK
+    return str(alerta.id), status.HTTP_200_OK
+
 
 @app.route('/removeralerta/<string:alert_id>', methods=['GET'])
 def remover_alerta(alert_id):
-  alerta = Alerta.objects.get(pk=alert_id)
+    alerta = Alerta.objects.get(pk=alert_id)
 
-  if alerta:
-      alerta.delete()
+    if alerta:
+        alerta.delete()
 
-  return "OK", status.HTTP_200_OK
+    return "OK", status.HTTP_200_OK
+
+
+@app.route('/enviaralerta', methods=['POST'])
+def enviar_alerta():
+    contact = request.data['contact']
+    alert_id = request.data['alert_id']
+
+    alerta = Alerta.objects.get(pk=alert_id)
+
+    if alerta:
+        alerta.send_alert(contact)
+
+    return "OK", status.HTTP_200_OK
+
 
 # Rodar servidor
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
